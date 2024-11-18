e8c92bf0fcc2d464e7667d63ab0f204cce225308
2vasy <2vasy@mail.ru>
2024-11-18 18:14:01 +0100

    add_basename="/marvel"

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -14,7 +14,7 @@ const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 const App = () => {
 
   return (
-    <Router>
+    <Router basename="/marvel">
       <div className="app">
         <AppHeader />
         <main>
